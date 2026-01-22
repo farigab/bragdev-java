5c17763b7a60bea5104a699619377347132bbe4b
Gabriel F <f-gab@outlook.com>
2026-01-22 16:29:36 -0300

    fix import

--- src/main/java/bragdoc/interfaces/api/auth/AuthController.java
+++ src/main/java/bragdoc/interfaces/api/auth/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
