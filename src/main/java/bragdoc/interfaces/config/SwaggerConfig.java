e8dfa37bc78dbcb2d94c4ab3a61e7a29ba509056
Gabriel F <f-gab@outlook.com>
2026-01-22 09:53:47 -0300

    feat: adiciona configuração do Swagger

--- src/main/java/bragdoc/interfaces/config/SwaggerConfig.java
+++ src/main/java/bragdoc/interfaces/config/SwaggerConfig.java
@@ -1,5 +1,23 @@
 package bragdoc.interfaces.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+
+@Configuration
 public class SwaggerConfig {
-    
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BragDoc API")
+                        .description("API para integração com GitHub e gerenciamento de conquistas")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("BragDoc")));
+    }
 }
